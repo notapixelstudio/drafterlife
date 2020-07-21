4c70141222339b6d8b2aecda18a1b800942f745c
nitaku <matteo.abrate@gmail.com>
2020-07-21 22:36:33 +0200

    simplistic card hover

--- godot/Card.gd
+++ godot/Card.gd
@@ -24,18 +24,19 @@ signal picked
 signal dropped
 
 var previous_mouse_position = Vector2()
-var is_dragging = false
+var dragging = false
 
 func _on_Card_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed('ui_touch'):
 		# Start dragging
 		get_tree().set_input_as_handled()
 		previous_mouse_position = event.position
-		is_dragging = true
+		dragging = true
+		set_hovering(false)
 		emit_signal('picked', self)
 
 func _input(event):
-	if not is_dragging:
+	if not dragging:
 		return
 		
 	if event is InputEventMouseMotion:
@@ -47,7 +48,7 @@ func _input(event):
 	if event.is_action_released('ui_touch'):
 		# End dragging
 		previous_mouse_position = Vector2()
-		is_dragging = false
+		dragging = false
 		
 		# Check if dropped onto target
 		for area in get_overlapping_areas():
@@ -62,11 +63,37 @@ func _input(event):
 # AUTOMATIC MOVEMENT
 ###
 
-onready var tween = $Tween
+onready var position_tween = $PositionTween
 
 func gracefully_go_to(point):
-	tween.interpolate_property(self, 'position',
+	position_tween.interpolate_property(self, 'position',
 		position, point, 0.5,
 		Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	tween.start()
+	position_tween.start()
 	
+###
+# HOVER
+###
+
+onready var scale_tween = $ScaleTween
+var hovering = false setget set_hovering
+
+func set_hovering(v):
+	hovering = v
+	
+	var target_scale
+	if hovering:
+		target_scale = 1.3*Vector2(1,1)
+	else:
+		target_scale = Vector2(1,1)
+	
+	scale_tween.interpolate_property(self, 'scale',
+		scale, target_scale, 0.25,
+		Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	scale_tween.start()
+
+func _on_Card_mouse_entered():
+	set_hovering(true)
+	
+func _on_Card_mouse_exited():
+	set_hovering(false)
